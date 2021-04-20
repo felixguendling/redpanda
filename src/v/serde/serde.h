b95293b3ec7c397f35e1f10b8a79bad2e1fa5c5e
Felix Gündling <felix.guendling@gmail.com>
2021-04-20 16:52:28 +0200

    serde: add datatypes

--- src/v/serde/serde.h
+++ src/v/serde/serde.h
@@ -42,29 +42,35 @@ namespace serde {
       : std::true_type {};                                                     \
                                                                                \
     template<typename T>                                                       \
-    inline constexpr auto const SERDE_TOKEN_PASTE(has_, name)                  \
-      = SERDE_TOKEN_PASTE(help_has_, name)<T>::value;
+    inline constexpr auto const SERDE_TOKEN_PASTE(                             \
+      has_, name) = SERDE_TOKEN_PASTE(help_has_, name)<T>::value;
 
 static auto iob = iobuf{};
 static auto iop = iobuf_parser{std::move(iob)};
 
-HAS_MEMBER_FN(serde_read, iop)
+HAS_MEMBER_FN(serde_read, iop, version_t{0}, version_t{0}, size_t{0U})
 HAS_MEMBER_FN(serde_write, iob)
-HAS_MEMBER_FN(serde_async_read, iop)
+HAS_MEMBER_FN(serde_async_read, iop, version_t{0}, version_t{0}, size_t{0U})
 HAS_MEMBER_FN(serde_async_write, iob)
 
 template<typename T>
-inline constexpr auto const is_serializable_v
-  = reflection::is_std_vector_v<T> || std::is_scalar_v<T> || is_envelope_v<T>;
+inline constexpr auto const is_serde_compatible_v =
+  is_envelope_v<T> || std::is_scalar_v<T> || std::is_enum_v<T> ||
+  reflection::is_std_vector_v<T> || reflection::is_named_type_v<T> ||
+  reflection::is_ss_bool_v<T> || std::is_same_v<T, std::chrono::milliseconds> ||
+  std::is_same_v<T, iobuf> || std::is_same_v<T, ss::sstring> ||
+  reflection::is_std_optional_v<T>;
+
+using header_t = std::tuple<version_t, version_t, size_t>;
+
+void write_envelope() {}
 
 template<typename T>
 void write(iobuf& out, T const& t) {
     using Type = std::decay_t<T>;
-    static_assert(is_serializable_v<Type>);
+    static_assert(has_serde_write<Type> || is_serde_compatible_v<Type>);
 
-    if constexpr (has_serde_read<Type>) {
-        t.write(out);
-    } else if constexpr (is_envelope_v<Type>) {
+    if constexpr (is_envelope_v<Type>) {
         write(out, Type::__version);
         write(out, Type::__compat_version);
 
@@ -72,62 +78,103 @@ void write(iobuf& out, T const& t) {
           sizeof(typename Type::envelope_size_t));
         auto const size_before = out.size_bytes();
 
-        envelope_for_each_field(t, [&out](auto& f) { write(out, f); });
+        if constexpr (has_serde_write<Type>) {
+            t.serde_write(out);
+        } else {
+            envelope_for_each_field(t, [&out](auto& f) { write(out, f); });
+        }
 
         auto const written_size = out.size_bytes() - size_before;
         if (
-          written_size
-          > std::numeric_limits<typename Type::envelope_size_t>::max()) {
+          written_size >
+          std::numeric_limits<typename Type::envelope_size_t>::max()) {
             throw std::system_error{
               make_error_code(error_codes::envelope_too_big)};
         }
         auto const size = ss::cpu_to_le(
           static_cast<typename Type::envelope_size_t>(written_size));
         size_placeholder.write(
-          reinterpret_cast<char const*>(&size), sizeof(size));
+          reinterpret_cast<char const*>(&size),
+          sizeof(typename Type::envelope_size_t));
+    } else if constexpr (std::is_enum_v<Type>) {
+        write(out, static_cast<std::underlying_type_t<Type>>(t));
     } else if constexpr (std::is_scalar_v<Type>) {
         auto le_t = ss::cpu_to_le(t);
         out.append(reinterpret_cast<char const*>(&le_t), sizeof(Type));
     } else if constexpr (reflection::is_std_vector_v<Type>) {
-        assert(t.size() <= std::numeric_limits<uint32_t>::max());
-        write(out, static_cast<uint32_t>(t.size()));
+        assert(t.size() <= std::numeric_limits<int32_t>::max());
+        write(out, static_cast<int32_t>(t.size()));
         for (auto const& el : t) {
             write(out, el);
+        }
+    } else if constexpr (reflection::is_named_type_v<Type>) {
+        return write(iob, static_cast<typename Type::type>(t));
+    } else if constexpr (reflection::is_ss_bool_v<Type>) {
+        write(iob, static_cast<int8_t>(bool(t)));
+    } else if constexpr (std::is_same_v<Type, std::chrono::milliseconds>) {
+        write<int64_t>(iob, t.count());
+    } else if constexpr (std::is_same_v<Type, iobuf>) {
+        write<int32_t>(out, t.size_bytes());
+        out.append(std::move(t));
+    } else if constexpr (std::is_same_v<Type, ss::sstring>) {
+        write<int32_t>(out, t.size());
+        out.append(t.data(), t.size());
+    } else if constexpr (reflection::is_std_optional_v<Type>) {
+        if (t) {
+            write(out, true);
+            write(out, std::move(t.value()));
+        } else {
+            write(out, false);
         }
     }
 }
 
 template<typename T>
+std::decay_t<T> read(iobuf_parser&);
+
+template<typename T>
+header_t read_header(iobuf_parser& in) {
+    using Type = std::decay_t<T>;
+
+    auto const version = read<version_t>(in);
+    auto const compat_version = read<version_t>(in);
+    auto const size = read<typename Type::envelope_size_t>(in);
+
+    if (compat_version > Type::__version) {
+        serde_log.error(
+          "read compat_version={} > {}::version={}\n",
+          static_cast<int>(compat_version),
+          type_str<T>(),
+          static_cast<int>(Type::__version));
+        throw std::system_error{
+          make_error_code(error_codes::version_older_than_compat_version)};
+    }
+
+    if (in.bytes_left() < size) {
+        serde_log.error(
+          "bytes_left={}, size={}\n", in.bytes_left(), static_cast<int>(size));
+        throw std::system_error{
+          make_error_code(error_codes::message_too_short)};
+    }
+
+    return std::make_tuple(version, compat_version, size);
+}
+
+template<typename T>
 std::decay_t<T> read(iobuf_parser& in) {
     using Type = std::decay_t<T>;
+    static_assert(has_serde_read<T> || is_serde_compatible_v<Type>);
+
     auto t = Type();
-
-    if constexpr (has_serde_read<Type>) {
-        t.read(in);
-    } else if constexpr (is_envelope_v<Type>) {
-        // Read envelope header: version, compat version, and size.
-        auto const version = read<version_t>(in);
-        auto const compat_version = read<version_t>(in);
-        auto const size = read<typename Type::envelope_size_t>(in);
-        (void)version; // could be used to drop messages from old versions
-
-        if (compat_version > Type::__version) {
-            serde_log.error(
-              "read compat_version={} > {}::version={}\n ",
-              static_cast<int>(compat_version),
-              type_str<T>(),
-              static_cast<int>(Type::__version));
-            throw std::system_error{
-              make_error_code(error_codes::version_older_than_compat_version)};
+    if constexpr (is_envelope_v<Type>) {
+        [[maybe_unused]] auto const [version, compat_version, size] =
+          read_header<Type>(in);
+        if constexpr (has_serde_read<Type>) {
+            t.serde_read(in, version, compat_version, size);
+        } else {
+            envelope_for_each_field(
+              t, [&](auto& f) { f = read<std::decay_t<decltype(f)>>(in); });
         }
-
-        if (in.bytes_left() < size) {
-            throw std::system_error{
-              make_error_code(error_codes::message_too_short)};
-        }
-
-        envelope_for_each_field(
-          t, [&](auto& f) { f = read<std::decay_t<decltype(f)>>(in); });
     } else if constexpr (std::is_scalar_v<Type>) {
         if (in.bytes_left() < sizeof(Type)) {
             throw std::system_error{
@@ -137,10 +184,23 @@ std::decay_t<T> read(iobuf_parser& in) {
         t = ss::le_to_cpu(in.consume_type<Type>());
     } else if constexpr (reflection::is_std_vector_v<Type>) {
         using value_type = typename Type::value_type;
-        t.resize(read<uint32_t>(in));
+        t.resize(read<int32_t>(in));
         for (auto i = 0U; i < t.size(); ++i) {
             t[i] = read<value_type>(in);
         }
+    } else if constexpr (reflection::is_named_type_v<Type>) {
+        t = read<typename Type::value_type>(in);
+    } else if constexpr (reflection::is_ss_bool_v<Type>) {
+        t = read<int8_t>(in);
+    } else if constexpr (std::is_same_v<Type, std::chrono::milliseconds>) {
+        t = std::chrono::milliseconds(read<int64_t>(in));
+    } else if constexpr (std::is_same_v<Type, iobuf>) {
+        return in.share(read<int32_t>(in));
+    } else if constexpr (std::is_same_v<Type, ss::sstring>) {
+        return in.read_string(read<int32_t>(in));
+    } else if constexpr (reflection::is_std_optional_v<Type>) {
+        return read<bool>(in) ? read<typename Type::value_type>()
+                              : std::nullopt;
     }
 
     return t;
@@ -150,8 +210,12 @@ template<typename T>
 ss::future<std::decay_t<T>> read_async(iobuf_parser& in) {
     using Type = std::decay_t<T>;
     if constexpr (has_serde_async_read<Type>) {
-        auto const t = std::make_shared<Type>();
-        return t->serde_async_read(in).then([t]() { return *t; });
+        auto const h = read_header<Type>(in);
+        return ss::do_with(Type{}, [&in, h](Type& t) {
+            auto const& [version, compat_version, size] = h;
+            return t.serde_async_read(in, version, compat_version, size)
+              .then([&t]() { return std::move(t); });
+        });
     } else {
         return ss::make_ready_future<std::decay_t<T>>(read<T>(in));
     }
@@ -160,8 +224,33 @@ ss::future<std::decay_t<T>> read_async(iobuf_parser& in) {
 template<typename T>
 ss::future<> write_async(iobuf& out, T const& t) {
     using Type = std::decay_t<T>;
-    if constexpr (has_serde_async_write<Type>) {
-        return t.serde_async_write(out);
+    if constexpr (is_envelope_v<Type> && has_serde_async_write<Type>) {
+        write(out, Type::__version);
+        write(out, Type::__compat_version);
+
+        auto size_placeholder = out.reserve(
+          sizeof(typename Type::envelope_size_t));
+        auto const size_before = out.size_bytes();
+
+        return t.serde_async_write(out).then(
+          [&out,
+           size_before,
+           size_placeholder = std::move(size_placeholder)]() mutable {
+              auto const written_size = out.size_bytes() - size_before;
+              if (
+                written_size >
+                std::numeric_limits<typename Type::envelope_size_t>::max()) {
+                  throw std::system_error{
+                    make_error_code(error_codes::envelope_too_big)};
+              }
+              auto const size = ss::cpu_to_le(
+                static_cast<typename Type::envelope_size_t>(written_size));
+              size_placeholder.write(
+                reinterpret_cast<char const*>(&size),
+                sizeof(typename Type::envelope_size_t));
+
+              return ss::make_ready_future<>();
+          });
     } else {
         write(out, t);
         return ss::make_ready_future<>();
